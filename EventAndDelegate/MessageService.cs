cc5923e416995650318449c6ca7ecc4d61dd851f
Haku-Home <gmkohaku@hotmail.com>
2019-06-11 21:07:10 +0800

    understand event and delegate

---
+++ EventAndDelegate/MessageService.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventAndDelegate
+{
+    public class MessageService
+    {
+        public void OnMonitorDone(object source, EventArgs args)
+        {
+            Console.WriteLine("Sending message...");
+        }
+    }
+}
---
+++ EventAndDelegate/Monitor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventAndDelegate
+{
+    public class Monitor
+    {
+
+        // 1- Define a delegate
+        // 2- Define an event based on that delegate
+        // 3- Raise event
+
+        public delegate void MonitorEventHandler(object source, EventArgs args);
+
+        public event MonitorEventHandler MonitorDone;
+
+        public void ProcessMonitor()
+        {
+            Console.WriteLine("Processing monitor...");
+            Thread.Sleep(2000);
+
+            OnMonitorDone();
+        }
+
+        protected virtual void OnMonitorDone()
+        {
+            MonitorDone?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
